8e411e2fc1b936eaffb4c55232f35b6aa8581c53
Xrui11 <871509389@qq.com>
2023-03-12 20:05:19 +0800

    更新模板

--- src/App.jsx
+++ src/App.jsx
@@ -6,28 +6,28 @@ function App() {
 
   return (
     <div className="wall">
-        <div style={{top: "0px",left: "0px"}}><img src="/imgs/1678610494809.jpg" alt=""/></div>
-        <div style={{top: "0px",left: "280px"}}><img src="/imgs/1678610530719.jpg" alt=""/></div>
-        <div style={{top: "320px",left: "40px"}}><img src="/imgs/1678610566190.jpg" alt=""/></div>
-        <div style={{top:"290px",left:"170px"}}><img src="/imgs/1678610594359.jpg" alt=""/></div>
-        <div style={{top:"240px",left:"330px"}}><img src="/imgs/1678610625796.jpg" alt=""/></div>
-        <div style={{top:"170px",left:"210px"}}><img src="/imgs/1678610642837.jpg" alt=""/></div>
-        <div style={{top:"450px",left:"240px"}}><img src="/imgs/1678610660011.jpg" alt=""/></div>
-        <div style={{top:"140px",left:"730px"}}><img src="/imgs/1678610674387.jpg" alt=""/></div>
-        <div style={{top:"490px",left:"10px"}}><img src="/imgs/1678610698828.jpg" alt=""/></div>
-        <div style={{top:"0px",left:"230px"}}><img src="/imgs/1678610716303.jpg" alt=""/></div>
-        <div style={{top:"120px",left:"560px"}}><img src="/imgs/1678610736523.jpg" alt=""/></div>
-        <div style={{top:"360px",left:"340px"}}><img src="/imgs/1678610775407.jpg" alt=""/></div>
-        <div style={{top:"230px",left:"460px"}}><img src="/imgs/1678610791194.jpg" alt=""/></div>
-        <div style={{bottom:"0px",left:"510px"}}><img src="/imgs/1678610821602.jpg" alt=""/></div>
-        <div style={{top:"30px",left:"1140px"}}><img src="/imgs/1678610848142.jpg" alt=""/></div>
-        <div style={{top:"240px",left:"1000px"}}><img src="/imgs/1678610862683.jpg" alt=""/></div>
-        <div style={{top:"150px",left:"800px"}}><img src="/imgs/1678610883104.jpg" alt=""/></div>
-        <div style={{top:"0px",left:"831px"}}><img src="/imgs/1678610895785.jpg" alt=""/></div>
-        <div style={{top:"0px",left:"540px"}}><img src="/imgs/1678610913396.jpg" alt=""/></div>
-        <div style={{top:"20px",left:"680px"}}><img src="/imgs/1678610931909.jpg" alt=""/></div>
-        <div style={{top:"0px",left:"960px"}}><img src="/imgs/1678610946387.jpg" alt=""/></div>
-        <div style={{bottom:"0px",right:"20px"}}><img src="/imgs/1678610968368.jpg" alt=""/></div>
+        <div className='img1' ><img src="/imgs/1678610494809.jpg" alt=""/></div>
+        <div className='img2' ><img src="/imgs/1678610530719.jpg" alt=""/></div>
+        <div className='img3' ><img src="/imgs/1678610566190.jpg" alt=""/></div>
+        <div className='img4' ><img src="/imgs/1678610594359.jpg" alt=""/></div>
+        <div className='img5' ><img src="/imgs/1678610625796.jpg" alt=""/></div>
+        <div className='img6' ><img src="/imgs/1678610642837.jpg" alt=""/></div>
+        <div className='img7' ><img src="/imgs/1678610660011.jpg" alt=""/></div>
+        <div className='img8' ><img src="/imgs/1678610674387.jpg" alt=""/></div>
+        <div className='img9' ><img src="/imgs/1678610698828.jpg" alt=""/></div>
+        <div className='img10' ><img src="/imgs/1678610716303.jpg" alt=""/></div>
+        <div className='img11' ><img src="/imgs/1678610736523.jpg" alt=""/></div>
+        <div className='img12' ><img src="/imgs/1678610775407.jpg" alt=""/></div>
+        <div className='img13' ><img src="/imgs/1678610791194.jpg" alt=""/></div>
+        <div className='img14' ><img src="/imgs/1678610821602.jpg" alt=""/></div>
+        <div className='img15' ><img src="/imgs/1678610848142.jpg" alt=""/></div>
+        <div className='img16' ><img src="/imgs/1678610862683.jpg" alt=""/></div>
+        <div className='img17' ><img src="/imgs/1678610883104.jpg" alt=""/></div>
+        <div className='img18' ><img src="/imgs/1678610895785.jpg" alt=""/></div>
+        <div className='img19' ><img src="/imgs/1678610913396.jpg" alt=""/></div>
+        <div className='img20' ><img src="/imgs/1678610931909.jpg" alt=""/></div>
+        <div className='img21' ><img src="/imgs/1678610946387.jpg" alt=""/></div>
+        <div className='img22' ><img src="/imgs/1678610968368.jpg" alt=""/></div>
     </div>
   )
 }
