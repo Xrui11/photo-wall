1e9e1ff413a73e04ab1cfb29a1a47b7e255764fa
okoala <dapixp@gmail.com>
2023-03-12 20:23:56 +0800

    feat: 更新图片

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div className="wall">
-        <div className='img1' ><img src="/imgs/1678610494809.jpg" alt=""/></div>
-        <div className='img2' ><img src="/imgs/1678610530719.jpg" alt=""/></div>
-        <div className='img3' ><img src="/imgs/1678610566190.jpg" alt=""/></div>
-        <div className='img4' ><img src="/imgs/1678610594359.jpg" alt=""/></div>
-        <div className='img5' ><img src="/imgs/1678610625796.jpg" alt=""/></div>
-        <div className='img6' ><img src="/imgs/1678610642837.jpg" alt=""/></div>
-        <div className='img7' ><img src="/imgs/1678610660011.jpg" alt=""/></div>
-        <div className='img8' ><img src="/imgs/1678610674387.jpg" alt=""/></div>
-        <div className='img9' ><img src="/imgs/1678610698828.jpg" alt=""/></div>
-        <div className='img10' ><img src="/imgs/1678610716303.jpg" alt=""/></div>
-        <div className='img11' ><img src="/imgs/1678610736523.jpg" alt=""/></div>
-        <div className='img12' ><img src="/imgs/1678610775407.jpg" alt=""/></div>
-        <div className='img13' ><img src="/imgs/1678610791194.jpg" alt=""/></div>
-        <div className='img14' ><img src="/imgs/1678610821602.jpg" alt=""/></div>
-        <div className='img15' ><img src="/imgs/1678610848142.jpg" alt=""/></div>
-        <div className='img16' ><img src="/imgs/1678610862683.jpg" alt=""/></div>
-        <div className='img17' ><img src="/imgs/1678610883104.jpg" alt=""/></div>
-        <div className='img18' ><img src="/imgs/1678610895785.jpg" alt=""/></div>
-        <div className='img19' ><img src="/imgs/1678610913396.jpg" alt=""/></div>
-        <div className='img20' ><img src="/imgs/1678610931909.jpg" alt=""/></div>
-        <div className='img21' ><img src="/imgs/1678610946387.jpg" alt=""/></div>
-        <div className='img22' ><img src="/imgs/1678610968368.jpg" alt=""/></div>
+      <div className="img1"></div>
+      <div className="img2"></div>
+      <div className="img3"></div>
+      <div className="img4"></div>
+      <div className="img5"></div>
+      <div className="img6"></div>
+      <div className="img7"></div>
+      <div className="img8"></div>
+      <div className="img9"></div>
+      <div className="img10"></div>
+      <div className="img11"></div>
+      <div className="img12"></div>
+      <div className="img13"></div>
+      <div className="img14"></div>
+      <div className="img15"></div>
+      <div className="img16"></div>
+      <div className="img17"></div>
+      <div className="img18"></div>
+      <div className="img19"></div>
+      <div className="img20"></div>
+      <div className="img21"></div>
+      <div className="img22"></div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
