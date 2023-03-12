9eb64711bf3e88b758b54799165a464bca7793d1
okoala <dapixp@gmail.com>
2023-03-12 20:42:57 +0800

    feat: 格式化代码

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { apiServerPlugin } from "./scripts/api-server";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    // apiServerPlugin({})
+    apiServerPlugin({
+      "/api/upload": async function (req) {
+
+      },
+    }),
   ],
-})
+});
